3517c17200eea036e27e571e79d6a68c6952df26
jinjinyun <jinyun.jin@gmail.com>
2016-11-09 22:32:15 +0800

    documents

--- lite/codemirror/build.js
+++ lite/codemirror/build.js
@@ -1,6 +1,10 @@
+
 var uglifyjs = require('uglifyjs');
 var fs = require('fs');
 var path = require('path');
+
+
+/*
 //var root = path.resolve(,'/');
 var list = fs.readdirSync(__dirname);
 var filename = __filename.replace(/.*[\/\\]/,'');
@@ -17,8 +21,48 @@ var source = list.map(function(f){
 	//console.log(s)
 
 }).join('\n');
-console.log(source)
+//console.log(source)
 fs.writeFileSync(__dirname+'/o.js',source);
 //console.log(uglifyjs.minify());
 //list.map()
+*/
+
+var from = '/Users/jinjinyun/Documents/workspace/node_modules/lite/doc/guide/'
+var dest = path.join(__dirname,'../');
+var base = "http://localhost:2012/doc/guide/";
+var http = require('http')
+
+var list = fs.readdirSync(from);
+list.map(function(n){
+	if(/\.(js|css)$/.test(n) || !/^layout/.test(n) && /.xhtml$/.test(n)){
+		var url = base+n
+		
+
+
+		http.get(url, (res) => {
+			var statusCode = res.statusCode;
+			var contentType = res.headers['content-type'];
+
+			var error;
+			if (statusCode !== 200) {
+				error = new Error(`Request Failed.\n` +
+				`Status Code: ${statusCode}`);
+			} 
+			if (error) {
+				console.log(error.message);
+				// consume response data to free up memory
+				res.resume();
+				return;
+			}
+
+			res.setEncoding('utf8');
+			var rawData = '';
+			res.on('data', (chunk) => rawData += chunk);
+			res.on('end', () => {
+				fs.writeFileSync(dest+'/'+n,rawData);
+			})
+		})
+	}
+	
+})
 
